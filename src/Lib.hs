ffc6bc6e463760f7e90d9e6f7d047c566cde2fa8
Tim Lamberton <tim.lamberton@gmail.com>
2016-09-08 21:59:19 +1000

    implement parser and syntax

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,5 +1,5 @@
 module Lib
-  ( someFunc
+  ( read
   ) where
 import Text.Parsec.String
   ( Parser
@@ -27,6 +27,21 @@ import Numeric
   , readOct
   )
   
+-- | My-language
+newtype MyNumber = MyNumber Double
+newtype MyIdent = MyIdent String
+data MyName = Ident MyIdent | Key MyRval
+newtype MyLocalRoute = MyLocalRoute [MyName]
+data MyRoute = Absolute MyIdent MyLocalRoute | Local MyLocalRoute
+data MyLval = Lroute MyRoute | Unnode MyUnnode
+data MyUnexpr = Unassign MyLocalRoute MyLval | Pack MyLval
+newtype MyUnnode = MyUnnode [MyUnexpr]
+data MyRval = Number MyNumber | String String | Rroute MyRoute | Node MyNode | App MyApp
+data MyExpr = Assign MyLval MyRval | Eval MyRval | Unpack MyRval
+newtype MyNode = MyNode [MyExpr]
+newtype MyApp = MyApp MyRval MyRval
+
+  
 -- | Parser that succeeds when consuming a string of underscore spaced digits
 integer d =
   do{ x <- d
@@ -34,44 +49,36 @@ integer d =
     ; return (x:xs)
     }
 
--- | My-language numeric type
-data MyNumber = MyI Integer
-              | MyF Double
-
 -- | Parser for valid floating point number
 float :: Parser MyNumber
 float =
   do{ xs <- integer digit
     ; y <- char '.'
     ; ys <- option "0" (integer digit)
-    ; return MyF $ read $ (xs ++ [y] ++ ys)
+    ; return MyNumber $ read $ (xs ++ [y] ++ ys)
     }
 
 -- | Parser for valid decimal number
 decimal :: Parser MyNumber
-decimal = optional (try $ string "0d") >> integer digit >>= return . MyI . read
+decimal = optional (try $ string "0d") >> integer digit >>= return . MyNumber . read
 
 -- | Parser for valid binary number
 binary :: Parser MyNumber
-binary = (try $ string "0d") >> integer (oneOf "01") >>= return . MyI . bin2dig
+binary = (try $ string "0d") >> integer (oneOf "01") >>= return . MyNumber . bin2dig
   where bin2dig = foldr (\x digint -> 2 * digint + (if x=='0' then 0 else 1)) 0
 
 -- | Parser for valid octal number
 octal :: Parser MyNumber
-octal = (try $ string "0o") >> integer octDigit >>= return . MyI . oct2dig
+octal = (try $ string "0o") >> integer octDigit >>= return . MyNumber . oct2dig
   where oct2dig = fst $ readOct x !! 0
 
 -- | Parser for valid hexidecimal number
 hexidecimal :: Parser MyNumber
-hexidecimal = (try $ string "0x") >> integer hexDigit >>= return . MyI . hex2dig
+hexidecimal = (try $ string "0x") >> integer hexDigit >>= return . MyNumber . hex2dig
   where hex2dig = fst $ readHex x !! 0
-
--- | My-language primitives
-data MyPrimitive = String String
-                 | Number MyNumber
                  
 -- | Parser for valid numeric value
-number :: Parser MyPrimitive
+number :: Parser MyRval
 number = (binary <|> octal <|> hexidecimal <|> float <|> decimal) >>= return . Number
 
 -- | Parser that succeeds when consuming an escaped character.
@@ -86,11 +93,9 @@ escapedChars =
                         }
 
 -- | Parser that succeeds when consuming a double-quote wrapped string.
-string :: Parser MyPrimitive
-string = between (char '"') (char '"') (many $ escapedChars <|> noneOf "\"\\") >>= return . String
+string :: Parser MyRval
+string = between (char '"') (char '"') (many $ escapedChars <|> noneOf "\"\\") >>= return . String . MyString
 
--- | My-language identifier
-data MyIdent = MyIdent String
 
 -- | Parser that succeeds when consuming a valid identifier string.
 ident :: Parser MyIdent
@@ -100,79 +105,110 @@ ident =
     ; return MyIdent (x:xs)
     }
 
+-- | Parse a valid node name
+name :: Parser MyName
+name =  ident >>= return . Ident
+    <|> between (char '(' >> spaces) (spaces >> char ')') rhs >>= return . Key
+
+
 -- | Parser that succeeds when consuming a non-newline space character
 nonbreak = notFollowedBy newline >> space
 
 -- | Parse an expression break
 break = many nonbreak >> (char ';' <|> newline) >> spaces
 
--- | My-language name
-data MyName = Ident MyIdent
-            | Key MyRVal
-
--- | Parse a valid node name
-name :: Parser MyName
-name =  ident >>= return . Ident
-    <|> between (char '(' >> spaces) (spaces >> char ')') rhs >>= return . Key
-
--- | My-language route
-data MyRoute = Route Ident [MyName]
-             | LocalRoute [MyName]
 
 -- | Parse a local route
-local_route :: Parser MyRoute
-local_route = many1 (char '.' >> name) >>= return . LocalRoute
+local_route :: Parser MyLocalRoute
+local_route = many1 (char '.' >> name) >>= return . MyLocalRoute
+
+-- | My-language route
 
 -- | Parse a route
 route :: Parser MyRoute
-route = local_route
-     <|> do{ x <- name
-           , LocalRoute y <- local_route
-           , return $ Route x y
+route = local_route >>= return . Local
+     <|> do{ x <- ident
+           ; y <- local_route
+           ; return $ Absolute x y
            }
 
--- | Parser that succeeds when consuming a node declaration
-node :: Parser MyVal
+lhs :: Parser MyLval
+lhs =  route >>= return . Lroute
+   <|> unnode >>= return . Unnode
+
+assign_unexpr :: Parser MyUnexpr
+assign_unexpr = 
+  do{ x <- local_route
+    ; spaces
+    ; char '='
+    ; spaces
+    ; y <- lhs
+    ; return $ Unassign x y
+    }
+
+pack_unexpr :: Parser MyUnexpr
+pack_unexpr = string "..." >> route >>= return . Lroute
+
+
+-- | Parse a destructuring expression
+unnode :: Parser MyUnnode
+unnode =
+  do{ char '{'
+    ; spaces
+    ; x <- unexpr
+    ; xs <- many (break >> unexpr)
+    ; ys <- option [] pack_expr
+    ; zs <- option [] (many (break >> unexpr))
+    ; return MyUnnode (xs ++ ys ++ zs)
+    }
+            
+-- | Parse an rvalue
+rhs :: Parser MyRval
+rhs =  string
+   <|> number
+   <|> route >>= return . Rroute
+   <|> node >>= return . Node
+
+-- | Parse an assignment expression
+assign_expr :: Parser MyExpr
+assign_expr =
+  do{ x <- lhs
+    ; spaces
+    ; char "="
+    ; spaces
+    ; y <- rhs
+    ; return Assign x y
+    }
+
+--- | Parse an eval expression
+eval_expr :: Parser MyExpr
+eval_expr = rhs >>= return . Eval
+
+-- | Parse an unpack expression
+unpack_expr :: Parser MyExpr
+unpack_expr = string "..." >> (   node >>= return . Unpack . Node
+                             <|> route >>= return . Unpack . Rroute
+                              )
+
+-- | Parse a curly-brace wrapped set of expressions
+node :: Parser MyNode
 node =
   do{ char '{'
-    ; optional (char '.')
-    ; try $
-      do
-        x <- lhs
-        char '='
-        return x
+    ; spaces
+    ; x <- expr
+    ; xs <- many (break >> expr)
+    ; spaces
+    ; return MyNode (x:xs)
     }
-    
--- | Parser that succeeds when consuming a valid lvalue
-lhs :: Parser MyVal
-lhs = ident
-   <|> between (char '(') (string <|> number)
+  where expr = assign_expr <|> eval_expr <|> unpack_expr
 
--- | Parser that succeeds when consuming a valid rvalue
-rhs :: Parser MyVal
-rhs = lhs
-  <|> string
-  <|> number
-  <|> node
+-- | Parse an application
+application :: Parser MyApp
+application =
+  do{ x <- rhs
+    ; y <- between (char '(' >> spaces) (spaces >> char ')') rhs
+    ; return MyApp x y
+    }
 
-
--- | Parser that succeeds when consuming a bracket-wrapped identifier or literal object
-index :: Parser String
-index =
-  do{ char '('
-    ; x <- ident <|> 
-    
--- | Parser that succeeds when consuming a valid member string.
-member :: Parser String
-member = char '.' >> ident
-
-
-
--- | My-language 
---
-data MyPrimitive = Ident String
-           | String String
-           | Number MyNumeric
-           | Node MyVal
 
 
