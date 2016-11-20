313d56d327b2b529b7b285a4b092cd8c04577e75
timbalam <tim.lamberton@gmail.com>
2016-11-21 08:29:43 +1000

    code compiles

--- src/Lib.hs
+++ src/Lib.hs
@@ -6,30 +6,32 @@ import Parser
   ( program
   )
 import Eval
- ( evalRval
- )
+  ( evalRval
+  )
 import Text.Parsec.String
   ( Parser
   )
 import Control.Monad.Except
- ( MonadError
- , throwError
- )
+  ( MonadError
+  , throwError
+  , runExceptT
+  )
   
+import qualified Types.Parser as T
 import qualified Text.Parsec as P
 import qualified Error as E
 import Types.Eval
- ( runEval
- )
+  ( runEval
+  )
 
 readMy :: MonadError E.Error m => Parser a -> String -> m a
 readMy parser input = either (throwError . E.Parser) return (P.parse parser "myc" input)
 
 readProgram :: String -> String
 readProgram s = either show showStmts (readMy program s)
-  
-showStmts (x:xs) = show x ++ foldr (\a b -> ";\n" ++ show a ++ b) "" xs
+  where
+    showStmts (x:xs) = show x ++ foldr (\a b -> ";\n" ++ show a ++ b) "" xs
   
 evalProgram :: String -> IO String
 evalProgram s =
-  runEval (readMy program s >>= evalRval) [] >>= return . either show showStmts
+  runExceptT (runEval (readMy program s >>= evalRval . T.Rnode) []) >>= return . either show show
