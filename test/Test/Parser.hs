2bab4a334c0e3ff4f35577d79af800595850d7cd
Tim Lamberton <tim.lamberton@gmail.com>
2017-05-11 23:13:31 +0200

    implementing io actions

--- test/Test/Parser.hs
+++ test/Test/Parser.hs
@@ -55,25 +55,27 @@ tests =
     , TestLabel "empty brackets" . TestCase $ assertError "empty bracket" "()" isParseError
     , TestLabel "identifier with applied brackets" . TestCase . assertParse "a.thing(applied)" $ wrap ((rident "a" `rref` "thing") `T.App` rident "applied")
     , TestLabel "identifier beginning with period with applied brackets" . TestCase . assertParse ".local(applied)" $ wrap (rsref "local" `T.App` rident "applied")
-    , TestCase . assertParse ".thing(a).get(b)" $ wrap (((rsref "thing" `T.App` rident "a") `rref` "get") `T.App` rident "b")
-    , TestCase . assertParse "-45" $ wrap (T.Unop T.Neg (T.Number 45))
-    , TestCase . assertParse "!hi" $ wrap (T.Unop T.Not (rident "hi"))
-    , TestCase . assertParse "this & that" $ wrap (rident "this" `_and_` rident "that")
-    , TestCase . assertParse "4 | 2" $ wrap (T.Number 4 `_or_` T.Number 2)
-    , TestCase . assertParse "10 + 3" $ wrap (T.Number 10 `_add_` T.Number 3)
-    , TestCase . assertParse "a + b + c" $ wrap ((rident "a" `_add_` rident "b") `_add_` rident "c")
-    , TestCase . assertParse "a - b" $ wrap (rident "a" `_sub_`rident "b")
-    , TestCase . assertParse "a + b - c" $ wrap ((rident "a" `_add_` rident "b") `_sub_` rident "c")
-    , TestCase . assertParse "a * 2" $ wrap (rident "a" `_prod_` T.Number 2)
-    , TestCase . assertParse "value / 2" $ wrap (rident "value" `_div_` T.Number 2)
-    , TestCase . assertParse "3^i" $ wrap (T.Number 3 `_pow_` rident "i")
-    , TestCase . assertParse "1 + 1 + 3 & 5 - 1" $ wrap (((T.Number 1 `_add_` T.Number 1) `_add_` T.Number 3) `_and_` (T.Number 5 `_sub_` T.Number 1))
-    , TestCase . assertParse "1 + 1 + 3 * 5 - 1" $ wrap (((T.Number 1 `_add_` T.Number 1) `_add_` (T.Number 3 `_prod_` T.Number 5)) `_sub_` T.Number 1)
-    , TestCase . assertParse "assign = 1" $ T.Rnode [lident "assign" `T.Assign` T.Number 1]
-    , TestCase . assertParse "undef =" $ T.Rnode [T.Declare (lident' "undef")]
-    , TestCase . assertParse "{ a = b }" $ wrap (T.Rnode [lident "a" `T.Assign` rident "b"])
-    , TestCase . assertParse "{ a = 1; b = a; c }" $ wrap (T.Rnode [lident "a" `T.Assign` T.Number 1, lident "b" `T.Assign` rident "a", T.Eval (rident "c")])
-    , TestCase $
+    , TestLabel "chained applications" . TestCase . assertParse ".thing(a).get(b)" $ wrap (((rsref "thing" `T.App` rident "a") `rref` "get") `T.App` rident "b")
+    , TestLabel "primitive negative number" . TestCase . assertParse "-45" $ wrap (T.Unop T.Neg (T.Number 45))
+    , TestLabel "boolean not" . TestCase . assertParse "!hi" $ wrap (T.Unop T.Not (rident "hi"))
+    , TestLabel "boolean and" . TestCase . assertParse "this & that" $ wrap (rident "this" `_and_` rident "that")
+    , TestLabel "boolean or" . TestCase . assertParse "4 | 2" $ wrap (T.Number 4 `_or_` T.Number 2)
+    , TestLabel "addition" . TestCase . assertParse "10 + 3" $ wrap (T.Number 10 `_add_` T.Number 3)
+    , TestLabel "multiple additions" . TestCase . assertParse "a + b + c" $ wrap ((rident "a" `_add_` rident "b") `_add_` rident "c")
+    , TestLabel "subtration" . TestCase . assertParse "a - b" $ wrap (rident "a" `_sub_`rident "b")
+    , TestLabel "mixed addition and subtraction" . TestCase . assertParse "a + b - c" $ wrap ((rident "a" `_add_` rident "b") `_sub_` rident "c")
+    , TestLabel "multiplication" . TestCase . assertParse "a * 2" $ wrap (rident "a" `_prod_` T.Number 2)
+    , TestLabel "division" . TestCase . assertParse "value / 2" $ wrap (rident "value" `_div_` T.Number 2)
+    , TestLabel "power" . TestCase . assertParse "3^i" $ wrap (T.Number 3 `_pow_` rident "i")
+    , TestLabel "mixed numeric and boolean operations" . TestCase . assertParse "1 + 1 + 3 & 5 - 1" $ wrap (((T.Number 1 `_add_` T.Number 1) `_add_` T.Number 3) `_and_` (T.Number 5 `_sub_` T.Number 1))
+    , TestLabel "mixed addition, subtration and multiplication" . TestCase . assertParse "1 + 1 + 3 * 5 - 1" $ wrap (((T.Number 1 `_add_` T.Number 1) `_add_` (T.Number 3 `_prod_` T.Number 5)) `_sub_` T.Number 1)
+    , TestLabel "assignment" . TestCase . assertParse "assign = 1" $ T.Rnode [lident "assign" `T.Assign` T.Number 1]
+    , TestLabel "declare" . TestCase . assertParse "undef =" $ T.Rnode [T.Declare (lident' "undef")]
+    , TestLabel "object with assignment" .  TestCase . assertParse "{ a = b }" $ wrap (T.Rnode [lident "a" `T.Assign` rident "b"])
+    , TestLabel "object with multiple statements" . TestCase . assertParse "{ a = 1; b = a; c }" $ wrap (T.Rnode [lident "a" `T.Assign` T.Number 1, lident "b" `T.Assign` rident "a", T.Eval (rident "c")])
+    , TestLabel "empty object" . TestCase $
+        assertParse "{}" (T.Rnode [])
+    , TestLabel "destructuring assignment" . TestCase $
         assertParse
           "{ .member = b } = object"
           (T.Rnode
@@ -81,8 +83,8 @@ tests =
                 [ plainsref "member" `T.ReversibleAssign` lident "b"]
               `T.Assign` rident "object"
             ])
-    , TestCase . assertParse "*b" $ T.Rnode [T.Unpack (rident "b")]
-    , TestCase $
+    , TestLabel "unpacked value" . TestCase . assertParse "*b" $ T.Rnode [T.Unpack (rident "b")]
+    , TestLabel "destructuring with final unpack statement" . TestCase $
         assertParse
           "{ .x = .val; *.y } = thing"
           (T.Rnode
@@ -92,7 +94,7 @@ tests =
                 ]
                 `T.Assign` rident "thing"
             ])
-    , TestCase $
+    , TestLabel "destructuring with beginning unpack statement" . TestCase $
         assertParse
           "{ *.y; .x = .out } = object"
           (T.Rnode
@@ -102,7 +104,7 @@ tests =
                 ]
               `T.Assign` rident "object"
             ])
-    , TestCase $
+    , TestLabel "destructuring with internal unpack statement" . TestCase $
         assertParse
           "{ .x = .val; *y; .z = priv } = other"
           (T.Rnode
