bcc312f3d9ea27cee077394f0b29ce2bebab70d2
timbalam <tim.lamberton@gmail.com>
2018-04-28 10:51:11 +0200

    type craziness

--- src/My/Types/Parser/Class.hs
+++ src/My/Types/Parser/Class.hs
@@ -1,14 +1,18 @@
-{-# LANGUAGE FlexibleInstances, FlexibleContexts, RankNTypes, MultiParamTypeClasses, FunctionalDependencies, GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE FlexibleInstances, FlexibleContexts, UndecidableInstances, RankNTypes, TypeFamilies, GeneralizedNewtypeDeriving #-}
 
 -- | Final encoding of my language syntax
 module My.Types.Parser.Class
   ( module My.Types.Parser
-  , LitExpr(..), OpExpr(..), VarExpr(..), GroupExpr(..), Syntax
-  , Field(..), Path(..)
-  , DeclStmt(..), LetRecStmt(..), Rec
-  , PunStmt(..), LetStmt(..), Ungroup(..), Tup, 
-  , LetPathPatt(..), Patt
-  , Program(..)
+  , Lit(..), Op(..)
+  , Tuple(..), Block(..), Group
+  , ATup(..), ABlock(..), Syntax
+  , Local(..), Self(..), Extern(..), Field(..), Path(..)
+  , AVarPath(..), ARelPath(..)
+  , Stmts(..), AGroup
+  , Decl(..), LetRec(..), ABlock(..), Rec
+  , PunStmt(..), LetStmt(..), ATup(..), Tup
+  , LetPath(..), Ungroup(..), APatt(..), Patt
+  , Program(..), AProgBody
   ) where
 import qualified Data.Text as T
 import Data.Semigroup (Semigroup)
@@ -40,25 +44,20 @@ class Op r where
   unop :: Unop -> r -> r
   binop :: Binop -> r -> r -> r
   
+-- | A path expression for my language recursively describes a set of
+-- nested fields relative to a self- or environment-defined field
 class Local r where
   local :: Ident -> r
   
 class Self r where
   self :: Key -> r
   
-class Import r where
+class Extern r where
   import_ :: Import -> r
-  
-class Group r where
-  group :: AGroup r -> r
-  extend :: r -> AGroup r -> r
   
 class Field r where
   at :: r -> Key -> r
   
-  
--- | A path expression for my language recursively describes a set of
--- nested fields relative to a self- or environment-defined field
 class (Local r, Field r) => LocalPath r
 instance (Local r, Field r) => LocalPath r
 
@@ -68,26 +67,44 @@ instance (Self r, Field r) => RelPath r
 class (Local r, Self r, Field r) => VarPath r
 instance (Local r, Self r, Field r) => VarPath r
 
+
+newtype ARelPath = ARelPath (forall r . RelPath r => r)
+instance Self ARelPath where
+  self x = ARelPath (self x)
+  
+instance Field ARelPath where
+  ARelPath r `at` k = ARelPath (r `at` k)
+
+newtype AVarPath = AVarPath (forall r . VarPath r => r)
+instance Local AVarPath where
+  local x = AVarPath (local x)
+  
+instance Self AVarPath where
+  self x = AVarPath (self x)
+  
+instance Field AVarPath where
+  at (AVarPath r) x = AVarPath (at r x)
+  
+  
+-- | The type of a value assigned in a name group
+type family Member r
+
+class Tuple r where
+  tup :: ATup (Member r) -> r
+  
+class Block r where
+  block :: ABlock (Member r) -> r
+  
+class Extend r where
+  type Group r
+  
+  extend :: r -> Group r -> r
+
   
 -- | Combination of syntax features 
-class (Lit r, Op r, VarPath r, Group r, Rec (Stmts r), Tup (Stmts r), Monoid (Stmts r r)) => Syntax r
-instance (Lit r, Op r, VarPath r, Group r, Rec (Stmts r), Tup (Stmts r), Monoid (Stmts r r)) => Syntax r where
+class (Lit r, Op r, VarPath r, Tuple r, Block r, Tuple (Group r), Block (Group r), Extend r, Member r ~ r) => Syntax r
+instance (Lit r, Op r, VarPath r, Tuple r, Block r, Tuple (Group r), Block (Group r), Extend r, Member r ~ r) => Syntax r
 
-
-
--- | Name groups
-class Group g where
-  tup :: ATup a -> g a
-  block :: ABlock a -> g a
-  
-newtype ATup a = ATup (forall s . (Tup s, Monoid (s a)) => s a)
-  deriving (Pun, Let)
-  
-newtype ABlock a = ABlock (forall s . (Rec s, Monoid (s a)) => s a)
-  deriving (Decl, LetRec)
-  
-newtype AGroup a = AGroup (forall g . Group g => g a)
-  deriving Group
 
 -- | Statements in a recursive group expression can be a
 --
@@ -96,19 +113,16 @@ newtype AGroup a = AGroup (forall g . Group g => g a)
 class Decl s where
   decl :: ARelPath -> s a
   
-class LetRec s where
-  letrec :: APatt -> a -> s a
+class Let s where
+  type Lhs s
   
-class (Decl s, LetRec s) => Rec s
-instance (Decl s, LetRec s) => Rec s
+  let_ :: Lhs s -> a -> s a
+  
+class (Decl s, Let s, Patt (Lhs s)) => Rec s
+instance Rec s
+  
+newtype ABlock a = ABlock (forall s . (Rec s, Monoid (s a)) => s a)
 
-  
-newtype ARelPath = ARelPath (forall r . RelPath r => r)
-instance Self ARelPath where
-  self x = ARelPath (self x)
-  
-instance Field ARelPath where
-  ARelPath r `at` k = ARelPath (r `At` k)
   
     
 -- | Statements in a tuple expression or decompose pattern can be a
@@ -122,15 +136,11 @@ instance Field ARelPath where
 class Pun s where
   pun :: AVarPath -> s a
   
-class Let s where
-  let_ :: ARelPath -> a -> s a
-  
-class (Pun s, Let s) => Tup s
-instance (Pun s, Let s) => Tup s
+class (Pun s, Let s, RelPath (Lhs s)) => Tup s
+instance Tup s
 
 
-newtype AVarPath = AVarPath (forall r . RelPath r => r)
-  deriving (Local, Self, Field)
+newtype ATup a = ATup (forall s . (Tup s, Monoid (s a)) => s a)
 
 
 -- | A pattern can appear on the lhs of a recursive let statement and can be a
@@ -139,18 +149,13 @@ newtype AVarPath = AVarPath (forall r . RelPath r => r)
 --   * Ungroup pattern (matches a set of paths to corresponding nested patterns)
 --   * An ungroup pattern with left over pattern (matches set of fields not
 --      matched by the ungroup pattern)
-class LetPath p where
-  letpath :: AVarPath -> p
+class (VarPath p, Tuple p, p ~ Member p, Extend p, Tuple (Group p), p ~ Group p) => Patt p
+instance Patt p
   
-class Ungroup p where
-  ungroup :: ATup p -> p
-  letungroup :: p -> ATup p -> p
+newtype APatt = APatt (forall p . Patt p => p)
+instance Local APatt where
+  local p = APatt (local p)
   
-class (LetPath p, Ungroup p) => Patt p
-instance (LetPath p, Ungroup p) => Patt p
-  
-newtype APatt = APatt (forall p . Patt => p)
-  deriving (LetPath, Ungroup)
 
 {-
 instance PathPatt Patt where
@@ -171,4 +176,8 @@ class (Local r, Self r) => Program r where
 
 -- | Group with at least one recursive statement
 newtype AProgBody a = AProgBody (forall s . (Rec s, Semigroup (s a)) => s a)
-  deriving (Decl, LetRec)
+instance Decl AProgBody where
+  decl p = AProgBody (decl p)
+  
+instance Let AProgBody where
+  let_ p a = AProgBody (let_ p a)
