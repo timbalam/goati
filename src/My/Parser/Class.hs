035788b0be1d1a820a26df7a41694ef6c60c7ef1
timbalam <tim.lamberton@gmail.com>
2018-04-29 00:03:24 +0200

    implement parsers for final syntax

--- src/My/Parser/Class.hs
+++ src/My/Parser/Class.hs
@@ -17,32 +17,19 @@ module My.Parser.Class
   )
   where
   
-import qualified My.Types.Parser.Class as C
+--import qualified My.Types.Parser.Class as C
 import My.Types.Parser.Class
-  ( LitExpr
-  , OpExpr
-  , VarExpr
-  , SubExpr
-  , GroupExpr
-  , Path
-  , Rec
-  , Tup
-  , Patt
-  , Program
+  ( Lit(..), Op(..), Self(..), Local(..), Extern(..), Field(..)
+  , Extend(..), Block(..), Tuple(..), Program(..)
+  , Let(..), RecStmt(..), TupStmt(..), Patt, Syntax, Member
+  , LocalPath, RelPath, VarPath
   )
 import My.Types.Parser
-  ( Ident(..)
-  , Key(..)
-  , Vis(..)
-  , Res(..)
-  , Import(..)
-  , Name
-  , Unop(..)
-  , Binop(..)
+  ( Ident(..), Key(..), Import(..)
+  , Unop(..), Binop(..)
   )
 import My.Parser
-  ( ShowMy(..)
-  , ReadMy(..)
+  ( ShowMy(..), ReadMy(..)
   , comment, spaces, point, stringfragment, escapedchars, identpath
   , eqsep, semicolonsep, ellipsissep, commasep
   , braces, parens, staples
@@ -66,7 +53,7 @@ import Control.Monad.State
      
     
 -- | Parse any valid numeric literal
-number :: C.LitExpr r => Parser r
+number :: Lit r => Parser r
 number =
   (binary
     <|> octal
@@ -77,29 +64,29 @@ number =
     
     
 -- | Parse a valid binary number
-binary :: C.LitExpr r => Parser r
+binary :: Lit r => Parser r
 binary =
   do
     try (P.string "0b")
-    C.integerLit . bin2dig <$> integer (P.oneOf "01")
+    int_ . bin2dig <$> integer (P.oneOf "01")
     where
       bin2dig =
         foldl' (\digint x -> 2 * digint + (if x=='0' then 0 else 1)) 0
 
         
 -- | Parse a valid octal number
-octal :: C.LitExpr r => Parser r
+octal :: Lit r => Parser r
 octal =
-  try (P.string "0o") >> integer P.octDigit >>= return . C.integerLit . oct2dig
+  try (P.string "0o") >> integer P.octDigit >>= return . int_ . oct2dig
     where
       oct2dig x =
         fst (readOct x !! 0)
 
         
 -- | Parse a valid hexidecimal number
-hexidecimal :: C.LitExpr r => Parser r
+hexidecimal :: Lit r => Parser r
 hexidecimal =
-  try (P.string "0x") >> integer P.hexDigit >>= return . C.integerLit . hex2dig
+  try (P.string "0x") >> integer P.hexDigit >>= return . int_ . hex2dig
   where 
     hex2dig x =
       fst (readHex x !! 0)
@@ -113,7 +100,7 @@ integer d =
    
     
 -- | Parser for valid decimal or floating point number
-decfloat :: C.LitExpr r => Parser r
+decfloat :: Lit r => Parser r
 decfloat =
   prefixed
     <|> unprefixed
@@ -121,7 +108,7 @@ decfloat =
     prefixed =
       do
         try (P.string "0d")
-        C.integerLit . read <$> integer P.digit
+        int_ . read <$> integer P.digit
         
     unprefixed =
       do
@@ -129,7 +116,7 @@ decfloat =
         fracnext xs                               -- int frac
                                                   -- int frac exp
           <|> expnext xs                          -- int exp
-          <|> (return . C.integerLit) (read xs)   -- int
+          <|> (return . int_) (read xs)   -- int
           
     fracnext xs =
       do 
@@ -138,12 +125,12 @@ decfloat =
         case m of
           Nothing ->
             -- frac
-            (return . C.numberLit . read) (xs ++ [y, '0'])
+            (return . num_ . read) (xs ++ [y, '0'])
             
           Just ys ->
             expnext (xs ++ [y] ++ ys)   -- frac exp
               <|>
-                (return . C.numberLit . read) (xs ++ [y] ++ ys)
+                (return . num_ . read) (xs ++ [y] ++ ys)
                                       -- frac
           
     expnext xs =
@@ -151,119 +138,133 @@ decfloat =
         e <- P.oneOf "eE"
         sgn <- P.option [] (P.oneOf "+-" >>= return . pure)
         ys <- integer P.digit
-        (return . C.numberLit . read) (xs ++ e:sgn ++ ys)
+        (return . num_ . read) (xs ++ e:sgn ++ ys)
 
 
 -- | Parse a double-quote wrapped string literal
-string :: C.LitExpr r => Parser r
+string :: Lit r => Parser r
 string =
-  C.stringLit . T.pack <$> stringfragment <?> "string literal"
+  str_ . T.pack <$> stringfragment <?> "string literal"
+  
+  
+lit :: Lit r => Parser r
+lit = number <|> string
 
   
+-- | Parse a variable name
+var :: (Local r, Self r, Extern r) => Parser r
+var = (local_ <$> readsMy)  -- alpha
+  <|> (self_ <$> readsMy)   -- '.' alpha
+  <|> (import_ <$> readsMy) -- '@'
+  
+  
+-- | Parse a field
+field :: Field r => r -> Parser r
+field x = at_ x <$> readsMy 
+  
+  
+-- | Parse zero or more fields
+path = iter field
 
--- | Parse a relative path
-path :: (Monad m, C.Path (m a)) => a -> Parser (m a)
-path = rest . pure
+iter :: (r -> Parser r) -> r -> Parser r
+iter step = rest
   where
-    rest x =
-      (readsMy >>= rest . C.at x)
+    rest x = 
+      (step x >>= rest)
         <|> return x
         
-instance (ReadMy a, Monad m, C.Path (m a)) => ReadMy (m a) where
-  readsMy = path . readsMy
+        
+relpath :: RelPath r => Parser r
+relpath = (self_ <$> readsMy) >>= path
+
+localpath :: LocalPath r => Parser r
+localpath = (local_ <$> readsMy) >>= path
 
 
-syntax :: C.Syntax r g (Name Ident Key Import) => Parser r
-syntax =
-    orexpr    -- '!' ...
-              -- '-' ...
-              -- '"' ...
-              -- '(' ...
-              -- digit ...
-              -- '{' ...
-              -- '.' alpha ...
-              -- alpha ...
-              -- '@' ...
-      <?> "expression"
-                    
+-- | Parse a value extension
+extend :: Extend r => r -> Parser (Group r) -> Parser r
+extend r p = ext_ r <$> p
+
           
 -- | Parse an expression observing operator precedence
-orexpr :: C.Syntax r g (Name Ident Key Import) => Parser r
-orexpr =
-  P.chainl1 andexpr (C.binop <$> readOr)
+orexpr :: Op r => Parser r -> Parser r
+orexpr term =
+  P.chainl1 (andexpr term) (binop_ <$> readOr)
 
-      
-andexpr :: C.Syntax r g (Name Ident Key Import) => Parser r
-andexpr =
-  P.chainl1 cmpexpr (C.binop <$> readAnd)
-
+andexpr :: Op r => Parser r -> Parser r
+andexpr term =
+  P.chainl1 (cmpexpr term) (binop_ <$> readAnd)
         
-cmpexpr :: C.Syntax r g (Name Ident Key Import) => Parser r
-cmpexpr =
+cmpexpr :: Op r => Parser r -> Parser r
+cmpexpr term =
   do
-    a <- addexpr
+    a <- addexpr term
     (do
        s <- op
-       b <- addexpr
+       b <- addexpr term
        return (s a b))
       <|> return a
   where
-    op = C.binop <$> (readGt <|> readLt <|> readEq <|> readNe <|> readGe <|> readLe)
-   
-   
-addexpr :: C.Syntax r g (Name Ident Key Import) => Parser r
-addexpr =
-  P.chainl1 mulexpr (C.binop <$> (readAdd <|> readSub))
+    op = binop_ <$> (readGt <|> readLt <|> readEq <|> readNe <|> readGe <|> readLe)
+      
+addexpr :: Op r => Parser r -> Parser r
+addexpr term =
+  P.chainl1 (mulexpr term) (binop_ <$> (readAdd <|> readSub))
 
+mulexpr :: Op r => Parser r -> Parser r
+mulexpr term =
+  P.chainl1 (powexpr term) (binop_ <$> (readProd <|> readDiv))
 
-mulexpr :: C.Syntax r g (Name Ident Key Import) => Parser r
-mulexpr =
-  P.chainl1 powexpr (C.binop <$> (readProd <|> readDiv))
-
-
-powexpr :: C.Syntax r g (Name Ident Key Import) => Parser r
-powexpr =
-  P.chainl1 term (C.binop <$> readPow)
-    where
-      term =
-        unop            -- '!'
-                        -- '-'
-          <|> pathexpr  -- '"'
-                        -- '('
-                        -- digit
-                        -- '{'
-                        -- '.'
-                        -- alpha
+powexpr :: Op r => Parser r -> Parser r
+powexpr term =
+  P.chainl1 term (binop_ <$> readPow)
+          
           
 -- | Parse an unary operation
-unop :: C.Syntax r g (Name Ident Key Import) => Parser r
-unop = C.unop <$> readsMy <*> pathexpr
-        
+unop :: Op r => Parser r -> Parser r
+unop term = unop_ <$> readsMy <*> term
+
+
+
+syntax :: Syntax r => Parser r
+syntax = orexpr term <?> "expression"
+  where
+    term =
+      unop pathexpr   -- '!' ...
+                      -- '-' ...
+        <|> pathexpr  -- '"' ...
+                      -- '(' ...
+                      -- digit ...
+                      -- '{' ...
+                      -- '.' alpha ...
+                      -- alpha ...
+                      -- '@' ...
+      
         
 -- | Parse a chain of field accesses and extensions
-pathexpr :: C.Syntax r g (Name Ident Key Import) => Parser r
+pathexpr :: Syntax r => Parser r
 pathexpr =
   first >>= rest
   where
     next x =
-      (C.extend x <$> group)
-        <|> (C.at x <$> syntax)
+      (extend x group)
+        <|> field x
     
-    
-    rest x =
-      (next x >>= rest)
-        <|> return x
-    
+    rest = iter next
     
     first =
-      string                      -- '"' ...
-        <|> parens disambigTuple  -- '(' ...
-        <|> (C.group <$> block)   -- '{' ...
-        <|> number                -- digit ...
-        <|> (C.var <$> readsMy)   -- '.' alpha ...
+      lit                         -- '"' ...
+                                  -- digit ...
+        <|> var                   -- '.' alpha ...
                                   -- alpha ...
                                   -- '@' ...
+        <|> parens disambigTuple  -- '(' ...
+        <|> block syntax          -- '{' ...
         <?> "value"
+        
+        
+    group :: Syntax r => Parser (Group r)
+    group = block syntax <|> tuple syntax
         
             
     -- | Handle a tricky parsing ambiguity between plain brackets and
@@ -276,71 +277,35 @@ pathexpr =
     --   disambiguate by looking next for an assignment `=` or a comma `,` 
     --   indicating a tuple expression. Otherwise we return rhs expression
     --   (and the calling function will then expect a closing paren).
-    disambigTuple :: C.Syntax r g (Name Ident Key Import) => Parser r
-    disambigTuple = (syntax >>= \ (TryStmt e) -> case e of 
-      Nothing -> syntax
-      Just (Pub p) ->
-        eqNext p                -- '=' ...
-          <|> sepNext (Pub p)   -- ',' ...
-          <|> syntax            -- ')' ...
-      Just (Priv p) ->
-        sepNext (Priv p)        -- ',' ...
-          <|> syntax)           -- ')' ...
-        <|> return (C.group mempty)
+    disambigTuple :: Syntax r => Parser r
+    disambigTuple =
+      (pubfirst                 -- '.' alpha
+        <|> privfirst           -- alpha
+        <|> pure (tup_ mempty))  -- ')'
       where
-        eqNext
-          :: C.Syntax r g (Name Ident Key Import)
-          => (forall p. Path p => p Key)
-          -> Parser r
-        eqNext p = liftA2 go (eqsep >> readsMy) tuple1 where
-          go x xs = C.group (C.let_ p x <> xs)
+        privfirst = do
+          p <- relpath
+          (tup_ <$> eqnext p         -- '=' ...
+            <|> tup_ <$> sepnext p   -- ',' ...
+            <|> rest p)              -- ')' ...
           
-        sepNext
-          :: C.Syntax r g (Name Ident Key Import)
-          => (forall p. Path p => Vis (p Ident) (p Key))
-          -> Parser r
-        sepNext p = C.group . (C.pun p <>) <$> tuple1
-  
-  
--- | Try to interpret an expression as the start of a tuple
---   statement
-newtype TryStmt p = TryStmt (Maybe (Vis (p Ident) (p Key)))
-
-instance C.LitExpr (TryStmt p) where
-  integerLit = TryStmt Nothing
-  stringLit = TryStmt Nothing
-  numberLit = TryStmt Nothing
-  
-instance C.OpExpr (TryStmt p) where
-  unop _ _ = Nothing
-  binop _ _ _ = Nothing
-  
-instance C.Path p => C.SubExpr (TryStmt p) where
-  get (TryStmt m) x = TryStmt (bimap (`C.at` x) (`C.at` x) m)
-  
-instance C.Path p => C.VarExpr (TryStmt p) (Name Ident Key Import) where
-  var x = case x of
-    Ex _ -> Nothing
-    In v -> Just (bimap pure pure v)
-    
-instance C.GroupExpr (TryStmt p) () where
-  group _ = Nothing
-  extend _ _ = Nothing
-
-        
--- | Parse a block expression
-group :: C.Syntax r g a => Parser g
-group = block syntax <|> tuple syntax
+        pubfirst = do
+          p <- localpath
+          (tup_ <$> sepnext p        -- ',' ...
+            <|> rest p)              -- ')' ...
+          
+        eqnext p = liftA2 (<>) (assign p syntax) tuple1
+        sepnext p = (p <>) <$> tuple1
     
         
 -- | Parse either an expression wrapped in parens or a tuple form block
-tuple :: (Tup g a, Monoid g) => Parser a -> Parser g
-tuple reads = parens (some <|> return mempty) <?> "tuple"
+tuple :: Tuple r => Parser (Member r) -> Parser r
+tuple reads = (tup_ <$> parens (some <|> return mempty)) <?> "tuple"
   where
     some = liftA2 (<>) (tupstmt reads) (tuple1 reads)
     
     
-tuple1 :: (Tup g a, Monoid g) => Parser a -> Parser g
+tuple1 :: (TupStmt s, Monoid s) => Parser (Rhs s) -> Parser s
 tuple1 reads = commasep >> msepEndBy (tupstmt reads) commasep
 
 
@@ -356,75 +321,65 @@ msepEndBy1 p sep =
       xs <- msepEndBy p sep
       return (x <> xs))
       <|> return x
-      
-    
-block :: (Rec g a, Monoid g) => Parser a -> Parser g
-block reads = braces (msepEndBy (recstmt reads) semicolonsep) <?> "block"
 
 
--- | Parse a statement of a block expression
-recstmt :: (ReadMy a, Rec g a) => Parser a -> Parser g
-recstmt reads = 
-    letrecstmt reads             -- '.' alpha ...
-                                 -- alpha ...
-      <|> destructurestmt reads  -- '(' ...
-      <?> "statement"
+-- | Assignment
+assign :: Let s => Lhs s -> Parser (Rhs s) -> Parser s
+assign l reads = let_ l <*> (eqsep >> reads)
 
     
 -- | Parse a statement of a tuple expression
-tupstmt :: Tup g a => Parser a -> Parser g
-tupstmt reads = do
-    v <- readsMy
-    case v of
-      Priv _ -> return (C.pun v)          -- alpha ...
-      Pub p ->                          -- '.' alpha ...
-        (C.let_ p <$> (eqsep >> reads))
-          <|> return (C.pun v)
-
-
--- | Parse a recursive let statement
-letrecstmt :: Rec g a => Parser a -> Parser g
-letrecstmt reads =
-  do
-    v <- readsMy
-    case v of
-      Pub p -> do                 -- '.' alpha ...
-        next (C.letpath v)
-          <|> return (C.decl p)
-          
-      Priv _ -> next (C.letpath v)  -- alpha ...
+tupstmt :: TupStmt s => Parser (Rhs s) -> Parser s
+tupstmt reads =
+  localpath       -- alpha ...
+    <|> pubfirst  -- '.' alpha ...
   where
-    next x = liftA2 C.letrec (destructure1 x) (eqsep >> reads)
-
-        
-
--- | Parse a destructuring statement
-destructurestmt :: Rec g a => Parser a -> Parser g
-destructurestmt reads =
-  do
-    l <- destructure
-    C.letrec l <$> (eqsep >> reads)
+    pubfirst = do
+      p <- relpath
+      assign p reads <|> return p
+      
+      
     
-                    
-                    
--- | Parse a valid lhs pattern for an assignment
-patt :: Patt p => Parser p
-patt = 
-  ((C.letpath <$> readsMy) >>= destructure1)
-    <|> destructure
-    <?> "set expression"
-        
+block :: Block r => Parser (Member r) -> Parser r
+block reads =
+  (block _ <$> braces (msepEndBy (recstmt reads) semicolonsep))
+    <?> "block"
+    
 
--- | Parse a destructure expression      
-destructure :: Patt p => Parser p
-destructure = C.ungroup <$> tuple patt >>= destructure1
+-- | Parse a statement of a block expression
+recstmt :: RecStmt s => Parser (Rhs s) -> Parser s
+recstmt reads =
+  pubfirst            -- '.' alpha ...
+    <|> privfirst     -- alpha ...
+    <|> ungroupfirst  -- '(' ...
+    <?> "statement"
+  where
+    pubfirst = do
+      p <- relpath
+      (next p          -- '('
+                       -- '='
+        <|> return p)  -- ';'
+                       -- '}'
+      
+    privfirst = localpath >>= next
+    ungroupfirst = ungroup >>= next
     
+    next p = do
+      p' <- extends p
+      assign p' reads
+      
+extends :: Patt p => p -> Parser p
+extends = iter (`extend` ungroup)
+      
+ungroup :: (Tuple p, Patt (Member p)) => Parser p
+ungroup = tuple patt
     
--- | Parse remaining chain of patterns
-destructure1 :: Patt p => p -> Parser p
-destructure1 x =
-  (tuple patt >>= destructure1 . C.letungroup x)
-    <|> return x
+patt :: Patt p => Parser p 
+patt = (do
+  p <- relpath    -- '.' alpha
+    <|> ungroup   -- '('
+  extends p)
+    <?> "pattern"
     
     
 -- | Parse a top-level sequence of statements
@@ -432,14 +387,14 @@ header :: Parser Import
 header = readsMy <* ellipsissep
 
 
-program :: C.Program r Import => Parser r
+program :: Program r => Parser r
 program = do
     m <- P.optionMaybe header
     x <- recstmt syntax
     (do
       semicolonsep
       xs <- msepEndBy (pure <$> recstmt syntax) semicolonsep
-      (return . C.program m) (option x (x<>) xs))
-      <|> return (C.program m x)
+      (return . prog_ m) (option x (x<>) xs))
+      <|> return (prog_ m x)
     <* P.eof
 
--- src/My/Types/Parser/Class.hs
+++ src/My/Types/Parser/Class.hs
@@ -1,18 +1,19 @@
-{-# LANGUAGE FlexibleInstances, FlexibleContexts, UndecidableInstances, RankNTypes, TypeFamilies, GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE FlexibleInstances, FlexibleContexts, RankNTypes, TypeFamilies #-}
 
 -- | Final encoding of my language syntax
+--
+-- A set of classes corresponding to particular syntactic language features
 module My.Types.Parser.Class
   ( module My.Types.Parser
   , Lit(..), Op(..)
-  , Tuple(..), Block(..), Group
-  , ATup(..), ABlock(..), Syntax
-  , Local(..), Self(..), Extern(..), Field(..), Path(..)
-  , AVarPath(..), ARelPath(..)
-  , Stmts(..), AGroup
-  , Decl(..), LetRec(..), ABlock(..), Rec
-  , PunStmt(..), LetStmt(..), ATup(..), Tup
-  , LetPath(..), Ungroup(..), APatt(..), Patt
-  , Program(..), AProgBody
+  , Local(..), Self(..), Extern(..), Field(..)
+  , Tuple(..), Block(..)
+  , Extend(..), Member
+  , LocalPath, RelPath, VarPath
+  , Syntax
+  , Let(..)
+  , RecStmt, TupStmt, Patt
+  , Program(..)
   ) where
 import qualified Data.Text as T
 import Data.Semigroup (Semigroup)
@@ -20,13 +21,9 @@ import My.Types.Parser
   ( Ident(..)
   , Key(..)
   , Import(..)
-  , Vis(..)
-  , Res(..)
-  , Name
   , Unop(..)
   , Binop(..)
   , StringExpr
-  , prec
   )
     
     
@@ -35,95 +32,84 @@ import My.Types.Parser
 -- This expression form closely represents the textual form of my language.
 -- After import resolution, it is checked and lowered and interpreted in a
 -- core expression form. See 'Types/Expr.hs'.
+class
+  ( Lit r, Op r, Local r, Self r, Extern r, Field r
+  , Tuple r, Block r, Member r ~ r
+  , Extend r, Tuple (Group r), Block (Group r), Member (Group r) ~ r
+  ) => Syntax r
+
+
+-- | Literal forms
 class Lit r where
-  integerLit :: Integer -> r
-  numberLit :: Double -> r
-  stringLit :: StringExpr -> r
+  int_ :: Integer -> r
+  num_ :: Double -> r
+  str_ :: StringExpr -> r
   
+-- | Unary and binary operators
 class Op r where
-  unop :: Unop -> r -> r
-  binop :: Binop -> r -> r -> r
-  
--- | A path expression for my language recursively describes a set of
--- nested fields relative to a self- or environment-defined field
+  unop_ :: Unop -> r -> r
+  binop_ :: Binop -> r -> r -> r
+
+-- | Use a environment-bound name
 class Local r where
-  local :: Ident -> r
+  local_ :: Ident -> r
   
+-- | Use a self-bound name
 class Self r where
-  self :: Key -> r
+  self_ :: Key -> r
   
+-- | Use an external name
 class Extern r where
   import_ :: Import -> r
   
+-- | Use a name of a component of a compound type
 class Field r where
-  at :: r -> Key -> r
+  at_ :: r -> Key -> r
   
+-- | A 'path' of nested fields of an environment-bound name
 class (Local r, Field r) => LocalPath r
-instance (Local r, Field r) => LocalPath r
 
+-- | A path of a self-bound name
 class (Self r, Field r) => RelPath r
-instance (Self r, Field r) => RelPath r
 
-class (Local r, Self r, Field r) => VarPath r
-instance (Local r, Self r, Field r) => VarPath r
-
-
-newtype ARelPath = ARelPath (forall r . RelPath r => r)
-instance Self ARelPath where
-  self x = ARelPath (self x)
+-- | A 'path' of either self- or environment-bound name
+class (RelPath r, LocalPath r) => VarPath r
   
-instance Field ARelPath where
-  ARelPath r `at` k = ARelPath (r `at` k)
-
-newtype AVarPath = AVarPath (forall r . VarPath r => r)
-instance Local AVarPath where
-  local x = AVarPath (local x)
-  
-instance Self AVarPath where
-  self x = AVarPath (self x)
-  
-instance Field AVarPath where
-  at (AVarPath r) x = AVarPath (at r x)
-  
-  
--- | The type of a value assigned in a name group
+-- | A value assignable in a name group
 type family Member r
-
-class Tuple r where
-  tup :: ATup (Member r) -> r
   
-class Block r where
-  block :: ABlock (Member r) -> r
+-- | Construct a tuple
+class (TupStmt (Tup r), Monoid (Tup r), Rhs (Tup r) ~ Member r) => Tuple r where
+  type Tup r
   
+  tup_ :: Tup r -> r
+  
+-- | Construct a block
+class (RecStmt (Rec r), Monoid (Rec r), Rhs (Rec r) ~ Member r) => Block r where
+  type Rec r
+  
+  block_ :: Rec r -> r
+  
+-- | Extend a value with a new name group
 class Extend r where
   type Group r
   
-  extend :: r -> Group r -> r
-
+  ext_ :: r -> Group r -> r
   
--- | Combination of syntax features 
-class (Lit r, Op r, VarPath r, Tuple r, Block r, Tuple (Group r), Block (Group r), Extend r, Member r ~ r) => Syntax r
-instance (Lit r, Op r, VarPath r, Tuple r, Block r, Tuple (Group r), Block (Group r), Extend r, Member r ~ r) => Syntax r
+-- | Assignment
+class Let s where
+  type Lhs s
+  type Rhs s
+  
+  let_ :: Lhs s -> Rhs s -> s
 
 
 -- | Statements in a recursive group expression can be a
 --
 --   * Declare statement (declare a path without a value)
 --   * Recursive let statement (define a pattern to be equal to a value)
-class Decl s where
-  decl :: ARelPath -> s a
-  
-class Let s where
-  type Lhs s
-  
-  let_ :: Lhs s -> a -> s a
-  
-class (Decl s, Let s, Patt (Lhs s)) => Rec s
-instance Rec s
-  
-newtype ABlock a = ABlock (forall s . (Rec s, Monoid (s a)) => s a)
+class (RelPath s, Let s, Patt (Lhs s)) => RecStmt s
 
-  
     
 -- | Statements in a tuple expression or decompose pattern can be a
 --
@@ -133,14 +119,7 @@ newtype ABlock a = ABlock (forall s . (Rec s, Monoid (s a)) => s a)
 --     a pattern)
 --
 --   TODO: Possibly allow left hand side of let statements to be full patterns
-class Pun s where
-  pun :: AVarPath -> s a
-  
-class (Pun s, Let s, RelPath (Lhs s)) => Tup s
-instance Tup s
-
-
-newtype ATup a = ATup (forall s . (Tup s, Monoid (s a)) => s a)
+class (VarPath s, Let s, RelPath (Lhs s)) => TupStmt s
 
 
 -- | A pattern can appear on the lhs of a recursive let statement and can be a
@@ -149,35 +128,15 @@ newtype ATup a = ATup (forall s . (Tup s, Monoid (s a)) => s a)
 --   * Ungroup pattern (matches a set of paths to corresponding nested patterns)
 --   * An ungroup pattern with left over pattern (matches set of fields not
 --      matched by the ungroup pattern)
-class (VarPath p, Tuple p, p ~ Member p, Extend p, Tuple (Group p), p ~ Group p) => Patt p
-instance Patt p
-  
-newtype APatt = APatt (forall p . Patt p => p)
-instance Local APatt where
-  local p = APatt (local p)
+class
+  ( VarPath p, Tuple p, Extend p
+  , Tuple (Group p), Member (Group p) ~ p, Member p ~ p
+  ) => Patt p
   
 
-{-
-instance PathPatt Patt where
-  pathSelf p = Patt (pathSelf p)
-  pathEnv p = Patt (pathEnv p)
-  
-instance UngroupPatt Patt where
-  ungroup tup = Patt (ungroup tup)
-  pathungroup (Patt patt) tup = Patt (pathungroup patt tup)
--}
-
-  
 -- | A program guaranteed to be a non-empty set of top level
 --  recursive statements with an optional initial global import
-class (Local r, Self r) => Program r where
-  program :: Maybe Import -> AProgBody r -> r
-   
-
--- | Group with at least one recursive statement
-newtype AProgBody a = AProgBody (forall s . (Rec s, Semigroup (s a)) => s a)
-instance Decl AProgBody where
-  decl p = AProgBody (decl p)
+class (RecStmt (Body r), Semigroup (Body r), Syntax (Rhs (Body r))) => Program r where
+  type Body r
   
-instance Let AProgBody where
-  let_ p a = AProgBody (let_ p a)
+  prog_ :: Maybe Import -> Body r -> r
