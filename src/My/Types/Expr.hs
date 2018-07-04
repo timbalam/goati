4a41ba3505b32c92acc06f3201eed45a8c2e4959
timbalam <tim.lamberton@gmail.com>
2018-07-04 21:29:52 +0200

    embed caching and primitives in evaluator

--- src/My/Types/Expr.hs
+++ src/My/Types/Expr.hs
@@ -44,10 +44,16 @@ data Expr k a =
     Prim (Prim (Expr k a))
   | Var a
   | Block (Defns k (Expr k) a)
+  | Expr k a `Update` Defns k (Expr k) a
+  | Self (Self k a) (Expr k a)
+  | Block (Defns k (Expr k) a)
   | Expr k a `At` k
   | Expr k a `Fix` k
-  | Expr k a `Update` Defns k (Expr k) a
   | Expr k a `AtPrim` (IOPrimTag (Expr k Void))
+  deriving (Functor, Foldable, Traversable)
+  
+  
+newtype Self k a = Self (M.Map k (Expr k a))
   deriving (Functor, Foldable, Traversable)
   
   
@@ -168,26 +174,27 @@ instance Ord k => Applicative (Expr k) where
 instance Ord k => Monad (Expr k) where
   return = Var
   
-  Prim p       >>= f = Prim ((>>= f) <$> p)
-  Var a        >>= f = f a
-  Block b      >>= f = Block (b >>>= f)
-  e `At` x     >>= f = (e >>= f) `At` x
-  e `Fix` m    >>= f = (e >>= f) `Fix` m
-  e `Update` b >>= f = (e >>= f) `Update` (b >>>= f)
-  e `AtPrim` p >>= f = (e >>= f) `AtPrim` p
+  Prim p          >>= f = Prim ((>>= f) <$> p)
+  Var a           >>= f = f a
+  Block b         >>= f = Block (b >>>= f)
+  e `Update` b    >>= f = (e >>= f) `Update` (b >>>= f)
+  Self m `Self` e >>= f = Self ((>>= f) <$> m) `Self` (e >>= f)
+  e `At` x        >>= f = (e >>= f) `At` x
+  e `Fix` m       >>= f = (e >>= f) `Fix` m
+  e `AtPrim` p    >>= f = (e >>= f) `AtPrim` p
   
 instance (Ord k, Eq a) => Eq (Expr k a) where
   (==) = eq1
   
 instance Ord k => Eq1 (Expr k) where
-  liftEq eq (Prim pa)        (Prim pb)        = liftEq (liftEq eq) pa pb
-  liftEq eq (Var a)          (Var b)          = eq a b
-  liftEq eq (Block ba)       (Block bb)       = liftEq eq ba bb
-  liftEq eq (ea `At` xa)     (eb `At` xb)     = liftEq eq ea eb && xa == xb
-  liftEq eq (ea `Fix` xa)    (eb `Fix` xb)    = liftEq eq ea eb && xa == xb
-  liftEq eq (ea `Update` ba) (eb `Update` bb) = liftEq eq ea eb && liftEq eq ba bb
-  liftEq eq (ea `AtPrim` pa) (eb `AtPrim` pb) = liftEq eq ea eb && pa == pb
-  liftEq _  _                   _               = False
+  liftEq eq (Prim pa)                (Prim pb)        = liftEq (liftEq eq) pa pb
+  liftEq eq (Var a)                  (Var b)          = eq a b
+  liftEq eq (Val _ (Block ba))       (Val _ (Block bb))       = liftEq eq ba bb
+  liftEq eq (Val _ (ea `Update` ba)) (Val _ (eb `Update` bb)) = liftEq eq ea eb && liftEq eq ba bb
+  liftEq eq (ea `At` xa)             (eb `At` xb)     = liftEq eq ea eb && xa == xb
+  liftEq eq (ea `Fix` xa)            (eb `Fix` xb)    = liftEq eq ea eb && xa == xb
+  liftEq eq (ea `AtPrim` pa)         (eb `AtPrim` pb) = liftEq eq ea eb && pa == pb
+  liftEq _  _                        _               = False
    
 instance (Ord k, Show k, Show a) => Show (Expr k a) where
   showsPrec = showsPrec1
@@ -278,18 +285,23 @@ instance Show1 Prim where
     
 instance Show (IOPrimTag a) where
   showsPrec i _ = errorWithoutStackTrace "show: IOPrimTag"
-       
+  
+  
+instance Ord k => Bound (Val k) where
+  Block b      >>>= f = Block (b >>>= f)
+  e `Update` b >>>= f = (e >>= f) `Update` (b >>>= f)
+  
+  
+instance (Ord k, 
+  
         
 instance Ord k => Bound (Defns k) where
   x >>>= f = go x
     where
       go (Defns fl en se) =
-        Defns fl ((>>>= f) <$$> en) ((>>>= f) <$$> se)
+        Defns fl (fmap (>>>= f) <$> en) (fmap (>>>= f) <$> se)
       go (Fields se) =
-        Fields ((>>= f) <$$> se)
-       
-      (<$$>) :: (Functor f, Functor g) => (a -> b) -> f (g a) -> f (g b)
-      (<$$>) = fmap . fmap
+        Fields (fmap (>>= f) <$> se)
   
   
 instance (Ord k, Eq1 m, Monad m) => Eq1 (Defns k m) where
