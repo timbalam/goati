f53cf56827a3834c7675408fa6eadcaaf1c9b9b1
Tim Lamberton <tim.lamberton@gmail.com>
2018-12-21 00:28:20 +0100

    simplified operator precedence

--- common/src/Goat/Syntax/Binop.hs
+++ common/src/Goat/Syntax/Binop.hs
@@ -14,77 +14,30 @@ infixl 7 #*, #/, :#*, :#/
 infixl 6 #+, #-, :#+, :#-
 infix 4 #==, #!=, #<, #<=, #>, #>=, :#==, :#!=, :#<, :#<=, :#>, :#>=
 
-data ArithOp a b =
+data AddOp a b =
     a :#+ b
   | a :#- b
-  | a :#* b
+  deriving (Eq, Show)
+  
+showAddOp :: (a -> ShowS) -> (b -> ShowS) -> AddOp a b -> ShowS
+showAddOp f g (a :#+ b) = showInfix f g Add a b
+showAddOp f g (a :#- b) = showInfix f g Sub a b
+  
+data MulOp a b =
+    a :#* b
   | a :#/ b
-  | a :#^ b
-  deriving (Eq, Ord, Show) 
+  deriving (Eq, Show)
   
-showArithOp :: (a -> ShowS) -> (b -> ShowS) -> ArithOp a b -> ShowS
-showArithOp f g (a :#+ b) = showInfix f g Add a b
-showArithOp f g (a :#- b) = showInfix f g Sub a b
-showArithOp f g (a :#* b) = showInfix f g Mul a b
-showArithOp f g (a :#/ b) = showInfix f g Div a b
-showArithOp f g (a :#^ b) = showInfix f g Pow a b
-
-data Precedence p q a b c =
-    DefaultLeft (q (p a b) c)
-  | DefaultRight (p a (q b c))
-  | NoDefault
+showMulOp :: (a -> ShowS) -> (b -> ShowS) -> ArithOp a b -> ShowS
+showMulOp f g (a :#* b) = showInfix f g Mul a b
+showMulOp f g (a :#/ b) = showInfix f g Div a b
   
-type Prec p q =
-    forall a b c
-  . (forall x . x -> p a x)
- -> (forall x . x -> q x c)
- -> b 
- -> Precedence p q a b c
-
-precArithOp :: Prec ArithOp ArithOp
-precArithOp f g b = case precl of
-  _ :#+ _ :#+ _ -> DefaultLeft precl
-  _ :#- _ :#+ _ -> DefaultLeft precl
-  _ :#+ _ :#- _ -> DefaultLeft precl
-  _ :#- _ :#- _ -> DefaultLeft precl
-  _ :#* _ :#+ _ -> DefaultLeft precl
-  _ :#* _ :#- _ -> DefaultLeft precl
-  _ :#/ _ :#+ _ -> DefaultLeft precl
-  _ :#/ _ :#- _ -> DefaultLeft precl
-  _ :#* _ :#* _ -> DefaultLeft precl
-  _ :#* _ :#/ _ -> DefaultLeft precl
-  _ :#/ _ :#* _ -> DefaultLeft precl
-  _ :#/ _ :#/ _ -> DefaultLeft precl
-  _ :#^ _ :#+ _ -> DefaultLeft precl
-  _ :#^ _ :#- _ -> DefaultLeft precl
-  _ :#^ _ :#* _ -> DefaultLeft precl
-  _ :#^ _ :#/ _ -> DefaultLeft precl
-  _             -> case precr of
-    _ :#+ _ :#* _ -> DefaultRight precr
-    _ :#- _ :#* _ -> DefaultRight precr
-    _ :#+ _ :#/ _ -> DefaultRight precr
-    _ :#- _ :#/ _ -> DefaultRight precr
-    _ :#+ _ :#^ _ -> DefaultRight precr
-    _ :#- _ :#^ _ -> DefaultRight precr
-    _ :#* _ :#^ _ -> DefaultRight precr
-    _ :#/ _ :#^ _ -> DefaultRight precr
-    _ :#^ _ :#^ _ -> DefaultRight precr
-    _             -> NoDefault
-  where
-    precl = g (f b)
-    precr = f (g b)
-    
+data PowOp a b =
+  a :#^ b
+  deriving (Eq, Show) 
   
-type BiPatt p = 
-  forall a b c . p a b -> ((forall x y . x -> y -> p x y) -> a -> b -> c) -> c
-
-pattArithOp :: BiPatt ArithOp
-pattArithOp p k = case p of
-  a :#+ b -> k (:#+) a b
-  a :#- b -> k (:#-) a b
-  a :#* b -> k (:#*) a b
-  a :#/ b -> k (:#/) a b
-  a :#^ b -> k (:#^) a b
+showPowOp :: (a -> ShowS) -> (b -> ShowS) -> ArithOp a b -> ShowS
+showPowOp f g (a :#^ b) = showInfix f g Pow a b
 
 showInfix
   :: (a -> ShowS) -> (b -> ShowS) -> Symbol -> a -> b -> ShowS
@@ -94,160 +47,30 @@ showInfix showa showb op a b =
     . showSymbol op
     . showChar ' '
     . showb b
+   
+data OpL p a =
+    LiftL a
+  | OpL (p (OpL p a) a)
 
-  
-data BiExpr p a =
-    BinPure a
-  | BinOp (p (BiExpr p a) (BiExpr p a))
-  deriving (Eq, Show)
-  
-  
-newtype DBiExpr p a = DBiExpr
-  { runDBiExpr
-      :: forall x y
-       . (a -> x)
-      -> (forall z . p (p x x) z -> p x z)
-      -> (forall z . p z (p x x) -> p z x)
-      -> (x -> y)
-      -> (p x x -> y)
-      -> y
-  }
-  
-dbiPure :: a -> DBiExpr p a
-dbiPure a = DBiExpr (\ kp _ _ xy _ -> xy (kp a))
+fromOpL
+ :: (forall x y . (x -> r) -> (y -> r) -> p x y -> r)
+ -> (a -> r) -> OpL p a -> r
+fromOpL fromp froma (LiftL a) = froma a
+fromOpL fromp froma (OpL p) = fromp (fromOpL fromp froma) froma p
 
-dbiOp
- :: (forall x y . x -> y -> p x y)
- -> DBiExpr p a
- -> DBiExpr p a
- -> DBiExpr p a
-dbiOp op (DBiExpr f) (DBiExpr g) = DBiExpr (\ kp kfl kfr xy py ->
-  f kp kfl kfr 
-
-
-  
-showsPrecBiExpr
- :: forall a
-  . Prec p p
- -> BiPatt p
- -> (a -> ShowS)
- -> (p ShowS ShowS -> ShowS)
- -> BiExpr p a
- -> ShowS
-showsPrecBiExpr _    _    showa _     (BinPure a) = showa a
-showsPrecBiExpr prec patt showa showp (BiExpr e) = showe e
-  where
-    showe :: p (BiExpr p a) (BiExpr p a) -> ShowS
-    showe = showp . showr . showl
-
-    showl :: forall c . p (BiExpr p a) c -> p ShowS c
-    showl p = patt p (\ op e c -> case e of 
-      BinPure a -> op (showa a) c
-      BinOp p' -> showsPrecL prec patt showe (op p' c))
-
-    showr :: forall b . p b (BiExpr p a) -> p b ShowS
-    showr p = patt p (\ op b e -> case e of
-      BinPure a -> op b (showa a)
-      BinOp p' -> showsPrecR prec patt showe (op b p'))
-
-  
-showsPrecDBiExpr
- :: forall a
-  . Prec p p
- -> BiPatt p
- -> (a -> ShowS)
- -> (p ShowS ShowS -> ShowS)
- -> DBiExpr p a
- -> ShowS
-showsPrecArith prec patt showa showp (DBiExpr f) = 
-  f showa showl showr id showp
-  where
-    showl :: forall c . p (p ShowS ShowS) c -> p ShowS c
-    showl p = showsPrecL prec patt showp p
-
-    showr :: forall b . p b (p ShowS ShowS) -> p b ShowS
-    showr = showsPrecR prec patt showp p
-
-
-showsPrecL
- :: Prec p
- -> BiPatt p
- -> (p a b -> ShowS)
- -> p (p a b) c
- -> p ShowS c
-showsPrecL prec patt showp p = patt p (\ op p' c ->
-  let
-    b = patt p' (\ op' a b -> case prec op op' a b c of
-        DefaultLeft _ -> False
-        _             -> True)
-  in  op (showParen b (showp p')) c)
-
-showsPrecR
- :: Prec p
- -> BiPatt p
- -> (p b c -> ShowS)
- -> p a (p b c)
- -> p a ShowS
-showsPrecR showp prec patt p = patt p (\ op a p' ->
-  let
-    b = patt p' (\ op' b c -> case prec op op' a b c of
-      DefaultRight _ -> False
-      _              -> True)
-  in op a (showParen b (showp p)))
-  
-  
--- | Parse an expression observing operator precedence
-parseBiExpr
- :: Prec p p
- -> Patt p
- -> Parser r
- -> Parser (DBiExpr p r -> DBiExpr p r -> DBiExpr p r)
- -> Parser (DBiExpr p r)
-parseBinExpr prec patt p op = do 
-  a <- p
-  go (dbiPure a)
-  where
-    go e = 
-      (do
-        f <- op
-        b <- p
-        goPrec (f e) (dbiPire b))
-        <|> return e
         
-        
-        
-        
-        
-            
-parsePrec prec patt p op f a =
-
-
- patt (f a) (\ op a b -> case e of
-  BinPure _ -> p
-  BinOp p'  -> patt p' (\ op' a b -> case prec op op' a b c of
-    DefaultLeft  _ -> p
-    DefaultRight pp -> patt pp (\ op a e -> BinOp (op a (BinOp e)))
-    NoDefault -> fail "operator precedence"))
-        
+data OpR p a =
+    LiftR a
+  | OpR (p a (OpR p a))
+  
+fromOpR
+ :: (forall x y . (x -> r) -> (y -> r) -> p x y -> r)
+ -> (a -> r) -> OpR p a -> r
+fromOpR fromp froma (LiftR a) = froma a
+fromOpR fromp froma (OpR p) = fromp froma (showOpR fromp froma) p
     
-  
-parseArith :: Arith_ r => Parser r -> Parser r
-parseArith p = parseAdd p where
-  parseAdd p = Parsec.chainl1 (parseMul p) addOp where 
-    addOp =
-      (parseSymbol Add >> return (#+))
-        <|> (parseSymbol Sub >> return (#-))
-        
-  parseMul p = Parsec.chainl1 (parsePow p) mulOp where
-    mulOp =
-      (parseSymbol Mul >> return (#*))
-        <|> (parseSymbol Div >> return (#/))
-        
-  parsePow p = Parsec.chainl1 p powOp where
-    powOp = parseSymbol Pow >> return (#^)
-
-
-type Arith = BinExpr ArithOp
+data Arith a =
+  ArithOp (OpL AddOp (OpL MulOp (OpR PowOp (Either a (Arith a)))))
 
 class Arith_ r where
   (#+) :: r -> r -> r
@@ -257,16 +80,31 @@ class Arith_ r where
   (#^) :: r -> r -> r
   
 instance Arith_ (Arith a) where
-  a #+ b = BinOp (a :#+ b)
-  a #- b = BinOp (a :#- b)
-  a #* b = BinOp (a :#* b)
-  a #/ b = BinOp (a :#/ b)
-  a #^ b = BinOp (a :#^ b)
+  a #+ b = ArithOp (add a :#+ mul b)
+  a #- b = ArithOp (add a :#- mul b)
+  a #* b = ArithOp (LiftL (mul a :#* pow b))
+  a #/ b = ArithOp (LiftL (mul a :#/ pow b))
+  a #^ b = ArithOp (LiftL (LiftL (eit a :#^ pow b)))
+  
+  add (ArithOp a) = a
+  
+  mul (ArithOp (LiftL a)) = a
+  mul a                   = LiftL (LiftR (Right a))
+  
+  pow (ArithOp (LiftL (LiftL a))) = a
+  pow a                           = LiftR (Right a)
+  
+  eit (ArithOp (LiftL (LiftL (LiftR a)))) = a
+  eit a                                   = Right a
   
   
 showArith
  :: forall a . (a -> ShowS) -> Arith a -> ShowS
-showArith showa = showsPrecBiExpr precArithOp pattArithOp showa (showArithOp id id)
+showArith showa (ArithOp opl) =
+  fromOpL showAddOp (fromOpL showMulOp (fromOpR showPowOp showEit)) opl
+  where 
+    showEit = either showa (showParen True . showArith showa)
+
   
   
 -- | Parse an expression observing operator precedence
@@ -287,17 +125,22 @@ parseArith p = parseAdd p where
 
     
 fromArith :: Arith_ r => Arith r -> r
-fromArith (ArithPure r) = r
-fromArith (ArithOp e) = case e of
-  a :#+ b -> fromArithOp (#+) a b
-  a :#- b -> fromArithOp (#-) a b
-  a :#* b -> fromArithOp (#*) a b
-  a :#/ b -> fromArithOp (#/) a b
-  a :#^ b -> fromArithOp (#^) a b
+fromArith (ArithOp opl) =
+  fromOpL fromAddOp (fromOpL fromMulOp (fromOpR fromPowOp fromEit)) opl
   where
-    fromArithOp
-     :: Arith_ r => (r -> r -> r) -> Arith r -> Arith r -> r
-    fromArithOp op a b = fromArith a `op` fromArith b
+    fromAddOp :: Arith_ r => (a -> r) -> (b -> r) -> AddOp a b -> r
+    fromAddOp f g (a :#+ b) = f a #+ f b
+    fromAddOp f g (a :#- b) = f a #- f b
+    
+    fromMulOp :: Arith_ r => (a -> r) -> (b -> r) -> MulOp a b -> r
+    fromMulOp f g (a :#* b) = f a #* f b
+    fromMulOp f g (a :#/ b) = f a #/ f b
+    
+    fromPowOp :: Arith_ r => (a -> r) -> (b -> r) -> PowOp a b -> r
+    fromPowOp f g (a :#^ b) = f a #^ g b
+    
+    fromEit :: Arith_ r => Either r (Arith r) -> r
+    fromEit = either id fromArith
     
     
 data CmpOp a b =
