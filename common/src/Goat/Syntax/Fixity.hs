e37169e4c8c2f7010dc2a811c25a26719f618f18
timbalam <tim.lamberton@gmail.com>
2019-01-18 22:25:46 +0100

    grammar and fixity types designed

--- common/src/Goat/Syntax/Fixity.hs
+++ common/src/Goat/Syntax/Fixity.hs
@@ -7,29 +7,28 @@ import Data.Bifunctor
 import Data.Coerce (coerce)
 import Control.Monad (ap)
 import Control.Monad.Trans.Free
-  
-data Op f a =
-    Term a
-  | Op (f a)
+
+
+newtype Wrap a = Wrap a
+
+newtype Emb f g = Emb (f (g a))
+ 
+newtype Exp a b = Term a | Exp b
   deriving (Eq, Show, Functor)
   
-hoistOp :: (forall x . f x -> g x) -> Op f a -> Op g a
-hoistOp f (Term a) = Term a
-hoistOp f (Op fa)  = Op (f fa)
-
-showOp
-  :: (forall x . (x -> ShowS) -> f x -> ShowS)
-  -> (a -> ShowS)
-  -> Op f a -> ShowS
-showOp sf sa (Term a) = sa a
-showOp sf sa (Op fa) = sf sa fa
+type Exp' f a b = Exp b (f (Exp a (Wrap b)))
 
 -- | A series of associated operations.
 -- 'p' is a binary operation that is associative in its second type argument.
---
--- e.g. InfixA (a `p` Term b)
--- InfixA (a `p` (Op (InfixA (a `p` Term b))))
--- InfixA (a `p` (Op (InfixA (a `p` (Op (InfixA (a `p` Term b)))))))
+newtype OpA p f a =
+  OpA (p (Exp (Exp a
+                   (Wrap (OpA p f a)))
+              (f (Exp a (Wrap (OpA p f a)))))
+         (Exp a
+              (Exp (OpA p f a)
+                   (f (Exp a (Wrap (OpA p f a)))))))
+  
+
 newtype InfixA p a b =
   InfixA { runInfixA :: p a (Op (InfixA p a) b) }
 
@@ -117,28 +116,35 @@ showInter sf sg sa (Inter f) =
     sfp sx f = showParen True (sf sx f)
 
 
-newtype Exp f g a =
-  Exp { runExp :: Op f (Inter g f a) }
+data Exp f g a =
+    ExpF (Inter f g a)
+  | ExpG (Inter g f a)
 
-instance (Eq (f (Inter g f a)), Eq (g (Op (Inter f g) a)))
+instance (Eq (f (Op (Inter g f) a)), Eq (g (Op (Inter f g) a)))
  => Eq (Exp f g a) where
-  Exp a == Exp b = a == b
+  ExpF a == ExpF b = a == b
+  ExpG a == ExpG b = a == b
+  _      == _      = False
   
-instance (Show (f (Inter g f a)), Show (g (Op (Inter f g) a)))
+instance (Show (f (Op (Inter g f) a)), Show (g (Op (Inter f g) a)))
  => Show (Exp f g a) where
-  showsPrec d (Exp a) = showParen (d>10)
-    (showString "Exp " . showsPrec 11 a)
+  showsPrec d (ExpF a) = showParen (d>10)
+    (showString "ExpF " . showsPrec 11 a)
+  showsPrec d (ExpG a) = showParen (d>10)
+    (showString "ExpG " . showsPrec 11 a)
     
 instance (Functor f, Functor g) => Functor (Exp f g) where
-  fmap f (Exp o) = Exp (fmap (fmap f) o)
+  fmap f (ExpF a) = ExpF (fmap f a)
+  fmap f (ExpG a) = ExpG (fmap f a)
     
 showExp
  :: (forall x . (x -> ShowS) -> f x -> ShowS)
  -> (forall x . (x -> ShowS) -> g x -> ShowS)
  -> (a -> ShowS)
  -> Exp f g a -> ShowS
-showExp sf sg sa (Exp o) =
-  showOp sf (showInter sg sfp sa) o
+showExp sf sg sa e = case e of
+  ExpF a -> showInter sf sg sa a
+  ExpG a -> showInter sg sfp sa a
   where
     sfp sx f = showParen True (sf sx f)
     
@@ -186,38 +192,17 @@ infixExp op (Exp a) (Exp b) =
 --  -> Exp (ExpA p) g a
 
 liftInfixExp
-  :: (g a -> g a -> g a)
-  -> Exp (ExpA p) g a
-  -> Exp (ExpA p) g a
-  -> Exp (ExpA p) g a
+  :: (forall x . g x -> g x -> g x)
+  -> Exp f g a
+  -> Exp f g a
+  -> Exp f g a
 liftInfixExp lop (Exp a) (Exp b) =
-  Exp (Term (lop (f a) (g b)))
+  Exp (Term (Inter (lop (f a) (f b))))
   where
-    f :: Op (ExpA p) (Inter g (ExpA p) a)
-      -> ?
-    
-    g :: Op (ExpA p) (Inter g (ExpA p) a)
-      -> ?
--- InfixA
---  :: p (ExpA p (Inter g (ExpA p) a))
---       (Op (InfixA p (ExpA p (Inter g (ExpA p) a)))
---           (Inter g (ExpA p) a))
---  -> InfixA p
---            (ExpA p (Inter g (ExpA p) a))
---            (Inter g (ExpA p) a)
--- Op
---  :: InfixA p 
---            (ExpA p (Inter g (ExpA p) a))
---            (Inter g (ExpA p) a)
---  -> Op (InfixA p (ExpA p (Inter g (ExpA p) a)))
---        (Inter g (ExpA p) a))
--- ExpA
---  :: Op (InfixA p (ExpA p (Inter g (ExpA p) a)))
---        (Inter g (ExpA p) a)
---  -> ExpA p (Inter g (ExpA p) a)
--- Op
---  :: ExpA p (Inter g (ExpA p) a)
---  -> Op (ExpA p) (Inter g (ExpA p) a)
+    f :: Op f (Inter g f a)
+      -> g (Op (Inter f g) a)
+    f (Term (Inter g)) = g
+    f (Op (
 -- Inter
 --   :: g (Op (Inter f g) a)
 --   -> Inter g f a
