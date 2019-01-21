578cfca6b0a767a6b1a1b986070c843338e62430
timbalam <tim.lamberton@gmail.com>
2019-01-21 20:40:14 +0100

    in progress

--- common/src/Goat/Syntax/Fixity.hs
+++ common/src/Goat/Syntax/Fixity.hs
@@ -8,16 +8,8 @@ import Data.Coerce (coerce)
 import Control.Monad (ap)
 import Control.Monad.Free
 
-
-type Exp = Free
-
-showExp
- :: (forall x . (x -> ShowS) -> f x -> ShowS)
- -> (a -> ShowS)
- -> Exp f a -> ShowS
-showExp sf sa (Pure a) = sa a
-showExp sf sa (Free fa) = sf (showExp sf sa) fa
   
+-- | Denotes a bracketed expression for nesting inside a higher precedence operation.
 newtype Wrap f a = Wrap (f a)
   deriving  (Eq, Show, Functor)
   
@@ -26,143 +18,196 @@ showWrap
  -> (a -> ShowS)
  -> Wrap f a -> ShowS
 showWrap sf sa (Wrap fa) = showParen True (sf sa fa)
+
+-- | Makes a binary operator associative in the second type variable position.
+--
+-- E.g. For 'data ROp a b = a `Rop` b',
+-- 'data LOp a b = b `Lop` a',
+-- 'data Op a b = a `Op` a';
+-- 'Assoc ROp f a b' is right-associative,
+-- 'Assoc LOp f a b'  is left-associative,
+-- and 'Assoc Op f a b' is non-associative.
+newtype AssocOp p f a b = AssocOp (p a (f b))
+
+instance (Bifunctor p, Functor f) => Functor (Assoc p f a) where
+  fmap = second
   
-data Embed f a b = Embed (f a) | Lift b
-  deriving (Eq, Show, Functor)
-  
-instance Functor f => Bifunctor (Embed f) where
-  bimap f g (Embed fa) = Embed (fmap f fa)
-  bimap f g (Lift b) = Lift (g b)
-  
-showEmbed
- :: (forall x . (x -> ShowS) -> f x -> ShowS)
+instance (Bifunctor p, Functor f) => Bifunctor (Assoc p f) where
+  bimap f g (AssocOp p) =
+    AssocOp (bimap f (fmap g) p)
+
+type Assoc p f a b = AssocOp p f a (Free (AssocOp p f a) b)
+
+hoistAssocOp
+ :: Bifunctor p
+ => (forall x . f x -> g x)
+ -> AssocOp p f a b -> AssocOp p g a b
+hoistAssocOp f (AssocOp p) = AssocOp (second f p)
+
+hoistAssoc
+ :: Bifunctor p
+ => (forall x . f x -> g x)
+ -> Assoc p f a b -> Assoc p g a b
+hoistAssoc f =
+  hoistAssoc f . fmap (hoistFree (hoistAssoc f))
+
+showAssocOp
+ :: (forall x y . (x -> ShowS) -> (y -> ShowS) -> p x y -> ShowS)
+ -> (forall x . (x -> ShowS) -> f x -> ShowS)
  -> (a -> ShowS)
  -> (b -> ShowS)
- -> Embed f a b -> ShowS
-showEmbed sf sa sb (Embed fa) = sf sa fa
-showEmbed sf sa sb (Lift b) = sb b
+ -> AssocOp p f a b -> ShowS
+showAssocOp sp sf sa sb (AssocOp p) = sp sa (sf sb) p
 
+showAssoc
+ :: (forall x y . (x -> ShowS) -> (y -> ShowS) -> p x y -> ShowS)
+ -> (forall x . (x -> ShowS) -> f x -> ShowS)
+ -> (a -> ShowS)
+ -> (b -> ShowS)
+ -> Assoc p f a b -> ShowS
+showAssoc sp sf sa sb =
+  showAssocOp sp sf sa
+    (iter (showAssocOp sp sf sa id) . fmap (sf sb))
 
--- |
-newtype Op p f a b = Op (p a (Embed f a b))
-  deriving (Eq, Show)
---  AssocOp (p (WExp f (AOp p f) a)
---             (AExp f (AOp p f) a))
-
-instance (Bifunctor p, Functor f) => Functor (Op p f a) where
-  fmap f (Op p) = Op (bimap f (first f) p)
+-- | Represents a possible layer of a functor 'f' for embedding nested operator expressions.
+data Emb f a b = Term a | Emb (f b)
+  deriving (Eq, Show, Functor)
   
-instance (Bifunctor p, Functor f) => Bifunctor (Op p f) where
-  bimap f g (Op p) = Op (bimap f (bimap f g) p)
-
--- | Associative binary operation.
-newtype Assoc p f a b = Assoc (Op p f b (Free (Op p f b) a))
-  deriving (Eq, Show)
+instance Functor f => Bifunctor (Emb f) where
+  bimap f g (Term a) = Term (f a)
+  bimap f g (Emb fb) = Emb (fmap g fb)
   
-instance (Bifunctor p, Functor f) => Functor (Assoc p f a) where
-  fmap f (Assoc o) =
-    Assoc (bimap f (hoistFree (first f) (fmap g o)))
+showEmb
+  :: (forall x . (x -> ShowS) -> f x -> ShowS)
+  -> (a -> ShowS)
+  -> (b -> ShowS)
+  -> Emb f a b -> ShowS
+showEmb sf sa sb (Term a) = sa a
+showEmb sf sa sb (Emb fb) = sf sb fb
 
-type WExp p f a = Free (Wrap (Assoc p f)) a
-type AExp p f a =
-  Embed f (WExp p f a) (Free (Op p f (WExp p f a)) a)
+-- | An associative operator expression,
+-- with a possible nested expression type 'f'.
+-- The 'b' type represents equal-or-higher precedence
+-- nested operations,
+-- and the 'a' type for nested operations of strictly higher precedence.
+newtype ExprOp p f a b =
+  ExprOp (Assoc p (Embed f a) (Embed f b b) b)
+  deriving (Eq, Show)
 
-showAExp
+instance (Bifunctor p, Functor f) => Functor (ExprOp p f a) where
+  fmap = second
+  
+instance (Bifunctor p, Functor f) => Bifunctor (ExprOp p f) where
+  bimap f g (ExprOp x) =
+    bimap (bimap g g)
+          g
+          (hoistAssoc (first f) x)
+
+showExprOp
+ :: (forall x y . (x -> ShowS) -> (y -> ShowS) -> p x y -> ShowS)
+ -> (forall x . (x -> ShowS) -> f x -> ShowS)
+ -> (a -> ShowS)
+ -> (b -> ShowS)
+ -> ExprOp p f a b -> ShowS
+showExprOp sp sf sa sb (ExprOp x) =
+  showAssoc sp (showEmb sf sb sb) (showEmb sf sa sb) x
+
+-- | An expression tree with possibly nested expressions embedded in a functor 'f'.
+-- 
+-- Makes the associative binary operation recursive in the
+-- first type position.
+type Exp p f a =
+  Embed f a
+    (Free (p (Embed f (WrapExp p f a) (WrapExp p f a)))
+          (Embed f a (WrapExp p f a)))
+          
+-- AssocOp p f a (Free (AssocOp p f a) b)
+-- p (Embed f (WrapExp p f a') (WrapExp p f a'))
+--   (Embed f a'
+--     (Free (AssocOp p (Embed f a')
+--              (Embed f (WrapExp p f a') (WrapExp p f a')))
+--           (WrapExp p f a')))
+          
+  Free (p (Embed f (WrapExp p f a) (WrapExp p f a)))
+       (Embed f a (WrapExp p f a)))
+type WrapExp p f a = Free (Wrap (ExprOp p f a)) a
+
+type ExpA p f a =
+  Either (p (Embed f (WrapExp p f a) (WrapExp p f a))
+            (Exp p f a))
+         (f (WrapExp p f a))
+
+
+
+type WrapExp p f a = Free (Wrap (Assoc p a) a)
+Op p (Emb f a (Free (Op p (Emb f a)) a))
+
+
+type WrapExp p f a = Free (Wrap (Assoc p f a)) a
+type ExpOp p f a =
+  Either (f (WrapExp p f a))
+         (Assoc p f a (WrapExp p f a))
+         
+type Exp p f a =
+  Either (f (WrapExp p f a))
+         (Free (Op p f (WrapExp p f a)) a)
+-- FreeT (Op p f (WrapExp p f a))
+--       (Either (f (WrapExp p f a)))
+--       a
+
+showExpOp
  :: (forall x . (x -> ShowS) -> (y -> ShowS) -> p x y -> ShowS)
  -> (forall x . (x -> ShowS) -> (f x -> ShowS))
  -> (a -> ShowS)
- -> AExp p f a -> ShowS
-showAExp sp sf =
-  showExp
-    (showEOp sf sg (showExp (showWOp sg)))
+ -> ExpOp p f a -> ShowS
+showExpOp sp sf sa =
+  either
+    (sf (showWrapExp sp sf sa))
+    (showAssoc sp sf sa (showWrapExp sp sf sa))
 
-showWExp
-  :: (forall x . (x -> ShowS) -> f x -> ShowS)
-  -> (forall x . (x -> ShowS) -> g x -> ShowS)
+showWrapExp
+  :: (forall x y . (x -> ShowS) -> (y -> ShowS) -> p x y -> ShowS)
+  -> (forall x . (x -> ShowS) -> f x -> ShowS)
   -> (a -> ShowS)
-  -> WExp f g a -> ShowS
-showWExp sf sg =
-  showExp
-    (showEOp sf (showWOp sg) (showExp (showWOp sg)))
-
--- | A series of associated operations.
--- 'p' is a binary operation that is associative in its second type argument.
-newtype AOp p f a =
-  AssocOp (p (WExp f (AOp p f) a)
-             (AExp f (AOp p f) a))
--- p (Either (m a) (f (m a)))
---   (Either (Either a b) (f (m a)))
-
-instance Eq (p (WExp f (AOp p f) a) (AExp f (AOp p f) a))
- => Eq (AOp p f a)
-  where
-    AssocOp pa  == AssocOp pb = pa == pb
- 
-instance Show (p (WExp f (AOp p f) a) (AExp f (AOp p f) a))
- => Show (AOp p f a)
-  where
-    showsPrec i (AssocOp p) = showParen (i>10)
-      (showString "AssocOp " . showsPrec 11 p)
-   
-instance (Bifunctor p, Functor f) => Functor (AOp p f) where
-  fmap f (AssocOp p) = AssocOp (bimap (fmap f) (fmap f) p)
-
-showAOp
- :: (forall x y . (x -> ShowS) -> (y -> ShowS) -> p x y -> ShowS)
- -> (forall x . (x -> ShowS) -> f x -> ShowS)
- -> (a -> ShowS)
- -> AOp p f a -> ShowS
-showAOp sp sf sa (AssocOp p) =
-  sp (showWExp sf (showAOp sp sf) sa)
-     (showAExp sf (showAOp sp sf) sa)
-     p
-
--- newtype Op p f a b = Op (p a (Either (f a) b))
--- newtype Assoc p f a b = Assoc (Op p f b (Free (Op p f b) a))
--- type WExp p f a = Free (Wrap (Assoc p f a)) a
--- type AExp p f a = Either (f (WExp p f a)) (Assoc p f a (WExp p f a))
-type AEOp p f = EOp f (AOp p f) (Exp (WOp (AOp p f)))
-
-showAEOp
- :: (forall x y . (x -> ShowS) -> (y -> ShowS) -> p x y -> ShowS)
- -> (forall x . (x -> ShowS) -> f x -> ShowS)
- -> (a -> ShowS)
- -> AEOp p f a -> ShowS
-showAEOp sp sf sa =
-  showEOp
-    sf
-    (showAOp sp sf)
-    (showExp (showWOp (showAOp sp sf)))
-    sa
+  -> WrapExp p f a -> ShowS
+showWrapExp sp sf sa =
+  iter (showWrap (showAssoc p f a) id) . fmap sa
         
     
-infixAExp
+infixExp
  :: (forall x y . x -> y -> p x y)
- -> AExp f (AOp p f) a
- -> AExp f (AOp p f) a
- -> AExp f (AOp p f) a
-infixAExp op a b =
-  Exp (LiftOp (AssocOp (op (f a) b)))
+ -> Exp p f a
+ -> Exp p f a
+ -> Exp p f a
+infixExp op a b =
+  Right (Free (op (fmap f a) b))
   where
-    f :: Exp (EOp f g h) a
-      -> Exp (EOp f (WOp g) h) a
-    f (Term a) = Term a
-    f (Exp (LiftOp aop)) = Exp (LiftOp (WrapOp aop))
-    f (Exp (EmbedOp fha)) = Exp (EmbedOp fha)
+    f :: Free (Op p f (WrapExp p f a)) a
+      -> Free (Wrap (Assoc p f a)) a
+      -- Exp (EOp f (WOp g) h) a
+    f (Pure a) = Pure a
+    f (Free o) = Free (Wrap (Assoc o))
  
-liftAExp
-  :: (forall x . Exp f x -> Exp f x -> Exp f x)
-  -> Exp (EOp f g (Exp (WOp g))) a
-  -> Exp (EOp f g (Exp (WOp g))) a
-  -> Exp (EOp f g (Exp (WOp g))) a
-liftAExp lop a b =
+liftExp
+  :: (forall x . Either ( x) (g -> Free f x -> Free f x)
+  -> Either (f (WrapExp p f a))
+            (Free (Op p f (WrapExp p f a)) a)
+  -> Exp p f a
+  -> Exp p f a
+liftExp lop a b =
   fout (lop (fin a) (fin b))
   where
     fin
-     :: Exp (EOp f g (Exp (WOp g))) a
-     -> Exp f (Exp (WOp g) a)
-    fin (Term a) = Term (Term a)
-    fin (Exp (LiftOp ga)) = Term (Exp (WrapOp ga))
+     :: Either (f (WrapExp p f a))
+               (Free (Op p f (WrapExp p f a)) a)
+        -- Exp (EOp f g (Exp (WOp g))) a
+     -> Free f (Free (Wrap (Assoc p f a)) a)
+        -- Exp f (Exp (WOp g) a)
+    fin (Right (Pure a)) = Pure (Pure a)
+    -- fin (Term a) = Term (Term a)
+    fin (Right (Free o)) = Pure (Free (Wrap (Assoc o)))
+    -- fin (Exp (LiftOp ga)) = Term (Exp (WrapOp ga))
+    fin (Left fex) = Free fex
     fin (Exp (EmbedOp fex)) = Exp fex
     
     fout
