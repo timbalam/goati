ba070cf1b83233e897935421670851ebf05cb268
timbalam <tim.lamberton@gmail.com>
2019-03-07 18:38:43 +0100

    implement syntax classes for expression type

--- common/src/Goat/Expr/Repr/Lang.hs
+++ common/src/Goat/Expr/Repr/Lang.hs
@@ -2,7 +2,7 @@
 module Goat.Expr.Repr.Lang
  where
 
-import Goat.Comp
+import Goat.Comp (run)
 import Goat.Expr.Pattern
 import Goat.Expr.Pattern.Lang
 import Goat.Expr.Repr
@@ -11,12 +11,13 @@ import Goat.Lang.LogicB (LogicB_(..))
 import Goat.Lang.ArithB (ArithB_(..))
 import Goat.Lang.CmpB (CmpB_(..))
 import Goat.Lang.Unop (Unop_(..))
-import Goat.Lang.Let (Let_(..))
+import Goat.Lang.Let (Let_(..), SomeDefn, fromDefn)
 import Goat.Lang.Field (Field_(..))
 import Goat.Lang.Extend (Extend_(..))
 import Goat.Lang.Block (Block_(..))
 import Goat.Lang.Ident (IsString(..))
 import Goat.Lang.Extern (Extern_(..))
+import Goat.Lang.Expr (SomeExpr, fromExpr)
 import Data.These
 
 import qualified Data.Text as Text
@@ -100,42 +101,49 @@ instance Field_ ReadExpr where
     readRel Self = Var (Left (Public ()))
 
 instance Block_ ReadExpr where
-  type Stmt ReadExpr = ReadStmt
+  type Stmt ReadExpr = ReadDefn
   block_ bdy = ReadExpr (Repr (Block abs))
     where
       abs =
         abstractDefinitions
           readExpr
-          (Multi (crosswalkDuplicates readStmt bdy))
+          (Multi (crosswalkDuplicates readDefn bdy))
   
       
 instance Extend_ ReadExpr where
   type Ext ReadExpr = ReadExpr
   (#) = readBinop (:#)
 
+readExprProof :: SomeExpr ReadDefn -> ReadExpr
+readExprProof = run . fromExpr id
 
-newtype ReadStmt =
-  ReadStmt {
-    readStmt
+
+newtype ReadDefn =
+  ReadDefn {
+    readDefn
      :: IdxBindings
           (Definitions These Assoc)
           (Pattern (Multi (Definitions These Assoc)))
           ReadExpr
     }
   
-punStmt :: Pun ReadChain ReadExpr -> ReadStmt
+punStmt :: Pun ReadChain ReadExpr -> ReadDefn
 punStmt = pun (setPattern . setPath . publicChain) id
     
-instance IsString ReadStmt where
+instance IsString ReadDefn where
   fromString s = punStmt (fromString s)
 
-instance Field_ ReadStmt where
-  type Compound ReadStmt =
+instance Field_ ReadDefn where
+  type Compound ReadDefn =
     Pun (Relative ReadChain) (Relative ReadExpr)
   r #. i = punStmt (r #. i)
 
-instance Let_ ReadStmt where
-  type Lhs ReadStmt = ReadPattern
-  type Rhs ReadStmt = ReadExpr
-  ReadPattern f #= a = ReadStmt (f a)
+instance Let_ ReadDefn where
+  type Lhs ReadDefn = ReadPattern
+  type Rhs ReadDefn = ReadExpr
+  ReadPattern f #= a = ReadDefn (f a)
+
+readDefnProof :: SomeDefn -> ReadDefn
+readDefnProof = run . fromDefn
+
 
